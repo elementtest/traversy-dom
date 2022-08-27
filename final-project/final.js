ac3ea7d4ec13dadc746edc79da385f4e143c5268
elementtest <elementtest7@gmail.com>
2022-08-27 21:56:06 +0800

    add readme whatise.txt

--- final-project/final.js
+++ final-project/final.js
@@ -1,10 +1,21 @@
 var form = document.getElementById('addForm');
 //box to left of black submit button 
+var filter = document.getElementById('filter');
+
+
+
+
+
 
 var itemList = document.getElementById('items');
 //list of items 1,2,3,4 etc with red boxes
 
 form.addEventListener('submit', addItem);
+
+itemList.addEventListener('click', removeItem);
+
+filter.addEventListener('keyup', filterItems);
+
 function addItem(e) {
    e.preventDefault();
 
@@ -16,8 +27,38 @@ function addItem(e) {
 
    li.appendChild(document.createTextNode(newItem));
 
+      
+   var deleteBtn = document.createElement('button');
+
+
+   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+   deleteBtn.appendChild(document.createTextNode('X'));
+
+   li.appendChild(deleteBtn);
+
+
+
    itemList.appendChild(li);
 
 
 
+}
+
+function removeItem(e){
+   if(e.target.classList.contains('delete')){
+      if(confirm('Are you sure?')){
+         var li = e.target.parentElement;
+         itemList.removeChild(li);
+         //item list is the list of items... you just remove child and remove child will remove the last child
+
+      }
+      //you are clicking the red button on the side.  the parent of the red button is the item3 or item2 or item1 whatever, so you are targeting that list item
+      //you have the little red button and you are targeting and taking out the parent. easy way to target instead of trying to mess around with like item3 or item4 id.  just say the parent of the red button boom it knows 
+   }
+}
+
+function filterItems(e){
+   var text = e.target.value.toLowerCase();
+   console.log(e);
 }
